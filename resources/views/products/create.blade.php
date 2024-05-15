570a3b32342b934063a08173673f9123bda71090
Mishkat Hossain <hossainmishkat0@gmail.com>
2024-05-15 20:56:26 +0600

    .

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -13,30 +13,30 @@
         <div class="container">
             <div class="row d-flex justify-content-center">
                 <div class="col-md-10">
-                    <div class="card border-0 shadow-lg my-3">
-                        <div class="card-header">
+                    <div class="card border-0 shadow-lg my-4">
+                        <div class="card-header bg-dark">
                             <h3>Create Product</h3>
                         </div>
                         <div class="card-body">
                                 <div class="mb-3">
-                                    <label for="" class='form-lable'>Name</label>
+                                    <label for="" class='form-label'>Name</label>
                                     <input type="text" class='form-control-lg' placeholder="Name" name='name'>
                                 </div>
                                 <div class="mb-3">
                                     <label for="" class='form-label'>Sku</label>
-                                    <input type="text" class='form-control-lg' placeholder="Name" name='name'>
+                                    <input type="text" class='form-control-lg' placeholder="Sku" name='sku'>
                                 </div>
                                 <div class="mb-3">
                                     <label for="" class='form-label'>Price</label>
-                                    <input type="text" class='form-control-lg' placeholder="Name" name='price'>
+                                    <input type="text" class='form-control-lg' placeholder="Price" name='price'>
                                 </div>
                                 <div class="mb-3">
                                     <label for="" class='form-label'>Description</label>
-                                    <input type="text" class='form-control-lg' placeholder="Name" name='description'>
+                                    <input type="text" class='form-control-lg' placeholder="Description" name='description'>
                                 </div>
                                 <div class="mb-3">
                                     <label for="" class='form-label'>Image</label>
-                                    <input type="file" class='form-control-lg' placeholder="Name" name='name'>
+                                    <input type="file" class='form-control-lg' placeholder="Image" name='name'>
                                 </div>
                         </div>
                         </div>
